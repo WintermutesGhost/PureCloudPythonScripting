ccf49339c27f22362719296d124ef6ac48028c0d
WintermutesGhost <wintermutes.ghost@protonmail.com>
2017-06-03 08:41:29 -0600

    Fixed borked request body for WebRTC phone creation

--- python/PureCloudUtils.py
+++ python/PureCloudUtils.py
@@ -122,11 +122,12 @@ def assignRoles(userId,roles):
 
 def createWebRtc(userId):
     name = getNameFromId(userId)
+    lineName = name.lower().replace(' ','') + "_webrtc"
     requestBody = {'name':name,
                    'site':{'name':'Calgary'},
                    'base':{'name':'WebRTC'},
-                   'web_rtc_user':{'id':userId}
-                   'lines':{'name':
+                   'web_rtc_user':{'id':userId},
+                   'lines':{'name':lineName}
                    }
     response = teleApi.post_telephony_providers_edges_phones(requestBody)
     return response
