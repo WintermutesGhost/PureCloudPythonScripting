109f709ad999fee82735fc90013f31989a69296c
WintermutesGhost <wintermutes.ghost@protonmail.com>
2017-06-15 15:28:09 -0600

    New analytics format built, pending testing

--- pctoolkit/analytics.py
+++ pctoolkit/analytics.py
@@ -28,7 +28,7 @@ def buildSimpleAQF(predicates: dict, filterType = 'and'):
     return aqFilter
 
 
-def buildPresenceQueryBody(interval,presenceFilters: list,routingFilters: list,userFilters: list, pageNumber = 1):
+def buildUserQueryBody(interval,presenceFilters: list,routingFilters: list,userFilters: list, pageNumber = 1):
     body = PureCloudPlatformClientV2.UserDetailsQuery()
     body.interval = interval
     if presenceFilters:
@@ -39,23 +39,6 @@ def buildPresenceQueryBody(interval,presenceFilters: list,routingFilters: list,u
         body.user_filters = userFilters
     body.paging = PureCloudPlatformClientV2.PagingSpec()
     body.paging.page_size = 100
-    body.paging.page_number = pageNumber #TODO: Better paging?
-    return body
-
-def buildUserQueryBody(interval,presenceFilters: dict,routingFilters: dict,userFilters: dict, pageNumber = 1):
-    body = PureCloudPlatformClientV2.UserDetailsQuery()
-    body.interval = interval
-    if presenceFilters:
-        pf = {k:v for k,v in presenceFilters.items()} #TODO:Put in right filters
-        body.presence_filters = [buildSimpleAQF(pf, 'or')]
-    if routingFilters:
-        rf = {k:v for k,v in routingFilters.items()}
-        body.routing_status_filters = [buildSimpleAQF(rf, 'or')]
-    if userFilters:
-        uf = {k:v for k,v in userFilters.items()}
-        body.user_filters = [buildSimpleAQF(uf, 'or')]
-    body.paging = PureCloudPlatformClientV2.PagingSpec()
-    body.paging.page_size = 100
     body.paging.page_number = pageNumber
     return body
 
@@ -63,11 +46,9 @@ def buildConversationQueryBody(interval,conversationFilters: list,segmentFilters
     body = PureCloudPlatformClientV2.ConversationQuery()
     body.interval = interval
     if conversationFilters:
-        cf = {'systemPresence':o for o in presenceFilters} #TODO: Put in right filters
-        body.conversation_filters = [buildSimpleAQF(f, 'or')]
+        body.conversation_filters = conversationFilters
     if segmentFilters:
-        rf = {'routingStatus':o for o in routingFilters}
-        body.segment_filters = [buildSimpleAQF(rf, 'or')]
+        body.segment_filters = segmentFilters
     body.paging = PureCloudPlatformClientV2.PagingSpec()
     body.paging.page_size = 100
     body.paging.page_number = pageNumber
