df4adab2ddda3612b352af3ad22f76493529bbf0
WintermutesGhost <wintermutes.ghost@protonmail.com>
2017-06-15 15:07:17 -0600

    temp work

--- pcscratch.py
+++ pcscratch.py
@@ -17,5 +17,17 @@ with open('C:\\Users\\mjsmi1\\timeOut.txt','w') as outFile:
     for l in outputTable:
         a=outFile.write(l+'\n')
 
+
+#Find short convs
 for conv in data['conversations']:
-    
+    outId = conv['conversationId']
+    for part in conv['participants']:
+        if part['purpose'] == 'agent':
+            for sess in part['sessions']:
+                for seg in sess['segments']:
+                    if seg['segmentType'] == 'interact':
+                        segS = dateutil.parser.parse(seg['segmentStart'])
+                        segE = dateutil.parser.parse(seg['segmentEnd'])
+                        segDelt = segE - segS
+                        if segDelt.seconds < 30:
+                            print(outId + "  |  " + str(segDelt.seconds))
