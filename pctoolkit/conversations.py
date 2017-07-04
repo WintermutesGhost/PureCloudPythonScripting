defe0eb9af157f1db2bf393c43b1607c6f9bc379
Michael J. Smith <wintermutes.ghost@protonmail.com>
2017-07-04 14:12:12 -0600

    Added list retrieval delay to avoid rate limits

--- pctoolkit/conversations.py
+++ pctoolkit/conversations.py
@@ -1,4 +1,5 @@
 import PureCloudPlatformClientV2
+import time # Only used to avoid hitting API limits
 #from pctoolkit.core import buildSimpleAQF,TODAY,YESTERDAY 
 
 convApi = PureCloudPlatformClientV2.apis.ConversationsApi()
@@ -15,3 +16,12 @@ def terminateCall(interactionId):
     convTemplate = {'state' : 'disconnected'}
     closedConv = convApi.patch_conversations_call(interactionId,convTemplate)
     return closedConv
+
+def getConversationList(conversationIds):
+    convList = []
+    for convId in conversationIds:
+        convList += convApi.get_conversation(convId)
+        # Sleep to avoid hitting API rate limits, pending a better solution
+        time.sleep(0.4) 
+        # TODO: Exception handling for graceful conversation not founds
+    return convList
