72f823094ee2bf423f22b98227cc575b3f43cfd1
Michael J. Smith <wintermutes.ghost@protonmail.com>
2017-07-10 13:47:43 -0600

    Cleanup and documentation

--- pctoolkit/conversations.py
+++ pctoolkit/conversations.py
@@ -1,23 +1,45 @@
-import PureCloudPlatformClientV2
 import time # Only used to avoid hitting API limits
-#from pctoolkit.core import buildSimpleAQF,TODAY,YESTERDAY 
 
+import PureCloudPlatformClientV2
+
+# Local reference for conversations API
 convApi = PureCloudPlatformClientV2.apis.ConversationsApi()
 
 def initiateCallFromMe(phoneNumber):
+    """
+    Initiates a call from the user currently authenticated
+    
+    Calls will not succeed unless the "Pleacing calls with another app?"
+    option is first enabled in the PureCloud web interface
+    
+    :param phoneNumber: phone number to place a call to
+    :returns: PureCloud conversation object for placed call
+    """
     callBody = {'phoneNumber' : phoneNumber}
     response = convApi.post_conversations_calls(callBody)
     return response
 
-def terminateCall(interactionId):
-    #activeConv = convApi.get_conversation(interactionId)
-    #activeConv.state = 'disconnected'
-    #convTemplate = PureCloudPlatformClientV2.Conversation
+def terminateCall(interactionId): #TODO: Rename to interaction, not just call
+    """
+    Disconnects all participants and ends an interaction
+    
+    :param interactionId: PureCloud interaction ID to disconnect
+    :returns: updated PureCloud conversation object for placed call
+    """
     convTemplate = {'state' : 'disconnected'}
     closedConv = convApi.patch_conversations_call(interactionId,convTemplate)
     return closedConv
 
-def getConversationList(conversationIds):
+def getConversationList(conversationIds): #TODO: renams
+    """
+    Returns PureCloud conversation objects for a list of conversation IDs
+    
+    Slow, as each request runs individually and requires 0.4 seconds to avoid
+    rate limits.
+    
+    :param conversationIds: iterable list of conversation IDs to retrieve
+    :returns: list of PureCloud conversation objects retrieved
+    """
     convList = []
     for convId in conversationIds:
         convList.append(convApi.get_conversation(convId))
