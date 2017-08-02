d88c9e9eedf3c31d50cdc63117470bc7770260fd
Michael J. Smith <wintermutes.ghost@protonmail.com>
2017-08-02 13:07:16 -0600

    Recordings deletion moved to work with iterable

--- pctoolkit/recordings.py
+++ pctoolkit/recordings.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import time
 
 import PureCloudPlatformClientV2
 
@@ -8,7 +9,7 @@ recApi = PureCloudPlatformClientV2.apis.RecordingApi()
 
 #TODO: Documentation
 
-def deleteInteractionRecordings(conversationId, deleteDate=None):
+def deleteInteractionRecordings(conversationIds, deleteDate=None):
     # If not specified, delete immediately
     # Time does not actually matter and is discarded by the system
     if deleteDate is None:
@@ -17,13 +18,22 @@ def deleteInteractionRecordings(conversationId, deleteDate=None):
     # TODO: Move out of function?
     deleteRecordingRecord = PureCloudPlatformClientV2.Recording()
     deleteRecordingRecord.delete_date = deleteDate
-    try:
-        recordings = recApi.get_conversation_recordings(conversationId)
-    except PureCloudPlatformClientV2.rest.ApiException:
-        logging.info('%s NO RECORDING FOUND', conversationId)
-    for rec in recordings:
-        response = recApi.put_conversation_recording(
-            conversationId,rec.id,deleteRecordingRecord)
-        logging.info('%s DELETION SET %s',
-                     conversationId, response.delete_date.date().isoformat())
+    for conversationId in conversationIds:
+        time.sleep(2)
+        try:
+            recordings = recApi.get_conversation_recordings(conversationId)
+        except PureCloudPlatformClientV2.rest.ApiException:
+            logging.info('%s NO RECORDING FOUND', conversationId)
+            continue
+        if len(recordings) == 0:
+            logging.info('%s NO RECORDING FOUND', conversationId)
+            continue
+        if recordings[0].file_state == 'DELETED':
+            logging.info('%s ALREADY DELETED', conversationId)
+            continue
+        for rec in recordings:
+            response = recApi.put_conversation_recording(
+                conversationId,rec.id,deleteRecordingRecord)
+            logging.info('%s DELETION SET %s',
+                         conversationId, response.delete_date.date().isoformat())
         
