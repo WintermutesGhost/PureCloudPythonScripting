e01ac4131013f20ecaa10639195f9b2f5d18a4e7
Michael J. Smith <wintermutes.ghost@protonmail.com>
2017-10-03 11:25:15 -0600

    Functions for pulling wrapupcodes genrally and from queues

--- pctoolkit/routing.py
+++ pctoolkit/routing.py
@@ -21,3 +21,26 @@ def getQueues():
     else:
         raise ValueError('Interval too long: More than 3000 results')
     return queueList
+
+def getWrapupCodes():
+    wrapupList = []
+    for page in range(1,30):
+        response = routApi.get_routing_wrapupcodes(page_size=100,page_number=page)
+        # Check if last page (empty response)
+        if len(response.entities) == 0 : break
+        wrapupList += response.entities
+    else:
+        raise ValueError('Interval too long: More than 3000 results')
+    return wrapupList
+
+def getQueueWrapupCodes(queueId):
+    wrapupList = []
+    for page in range(1,30):
+        response = routApi.get_routing_queue_wrapupcodes(queueId,
+                                                         page_size=100,
+                                                         page_number=page)
+        if len(response.entities) == 0: break
+        wrapupList += response.entities
+    else:
+        raise ValueError('Interval too long: More than 3000 results')
+    return wrapupList
